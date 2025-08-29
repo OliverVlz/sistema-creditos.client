284ad905aa33d5384ac2ad094b8be06bf0049e21
Oliver Velasquez <oliver.vlz01@gmail.com>
2025-08-29 16:05:53 -0500

    contact section

--- src/modules/landing/pages/HomePage/components/ContactSection.tsx
+++ src/modules/landing/pages/HomePage/components/ContactSection.tsx
@@ -120,7 +120,7 @@ export default function ContactSection() {
               <div className="flex items-center gap-3 mb-8">
 
                 <h3 className="text-2xl md:text-3xl font-semibold text-global-12 leading-tight">
-                  Solicita asesoría para tu crédito por libranza
+                  Solicita asesoría para tu crédito por libranza 
                 </h3>
               </div>
 
